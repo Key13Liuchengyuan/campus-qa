609cb3c3fe0870d580dcfc42630438e3a6c1069b
H07828 <3038056423@qq.com>
2026-08-25 21:15:17 +0800

    Update index.js

---
+++ src/features/qa/index.js
@@ -0,0 +1,10 @@
+import './style.css';
+
+export function initQa(container) {
+  container.innerHTML = `
+    <div class="qa-page">
+      <h2>问答聊天页面</h2>
+      <div class="chat-box"></div>
+    </div>
+  `
+}
